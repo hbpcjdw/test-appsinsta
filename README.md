ca2310857ca41a4bc1df2561942da797c229ccf6
Herlambang B.Pz <herlambangbp123@gmail.com>
2026-03-17 23:45:03 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Instagram Clone
+
+## ScreenShot
+
+<img src="assets/screenshots/ss1.jpg" height="500em"/>&nbsp;<img src="assets/screenshots/ss2.jpg" height="500em" />
+&nbsp;<img src="assets/screenshots/ss3.jpg" height="500em" /><img src="assets/screenshots/ss4.jpg" height="500em"/>&nbsp;<img src="assets/screenshots/ss3.jpg" height="500em" />
+&nbsp;<img src="assets/screenshots/ss6.jpg" height="500em" /><img src="assets/screenshots/ss7.jpg" height="500em"/>&nbsp;<img src="assets/screenshots/ss6.jpg" height="500em" />
+&nbsp;<img src="assets/screenshots/ss9.jpg" height="500em" />
---
+++ src/views/HomePage.vue
@@ -0,0 +1,30 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar color="primary">
+        <ion-title>Daftar Mahasiswa</ion-title>
+      </ion-toolbar>
+      <ion-toolbar color="primary">
+        <ion-searchbar 
+          placeholder="Cari nama mahasiswa..." 
+          v-model="searchQuery"
+          :debounce="300">
+        </ion-searchbar>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+/* Tambahkan styling khusus jika diperlukan */
+ion-avatar {
+  --border-radius: 8px;
+}
+</style>
